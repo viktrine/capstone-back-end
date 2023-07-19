29e69f85581b74c58ff39eff1f44e62ae72181cc
rkinyua5 <rkinyu5@gmail.com>
2023-07-19 19:26:37 +0300

    update users

---
+++ models/usermodel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const userschema = new mongoose.Schema({
+    "name":{type:String},
+    "email":{type:String},
+    "password":{type:String},
+    "phone":{type:String}
+});
+
+const usermodel = mongoose.model("user",userschema);
+module.exports=usermodel;
