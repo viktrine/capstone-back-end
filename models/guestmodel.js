409dc142656c533a925c6f4eb6086aa5ad8c7154
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-20 16:57:32 +0300

    Add few chnages

---
+++ models/guestmodel.js
@@ -0,0 +1,16 @@
+// get mongoose
+const { default: mongoose } = require("mongoose");
+
+// create a schema
+const guestschema = new mongoose.Schema({
+    "name":{type: String, required: true},
+    "phone":{type: String, required: true},
+    "host":{type: String, required: true},
+    "id":{type: String, required: true},
+    "event":{type: String, required: true}
+});
+
+// define model
+const guestModel = mongoose.model("guest", guestschema);
+
+module.exports = guestModel;
