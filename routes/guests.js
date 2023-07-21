10e2251b55e1fd8bf0570cbfa8d68b8db43424d2
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-21 17:42:03 +0300

    Add guest

--- routes/guests.js
+++ routes/guests.js
@@ -1,13 +1,31 @@
 // import express
 const express = require('express');
 
+// get model
+const guestModel = require('../models/guestmodel');
+
+
 // defn=ine router
 const guestRouter = express.Router();
 
-// logic here
+// add guest
+guestRouter.post("/", async function(req, res, next){
+    // get the number of existing guests
+    const nextId = await guestModel.count() + 1;
 
-guestRouter.get("/", function(req, res, next){
-    res.send("Get all guests under construction");
+    const guest = req.body;
+    guest.id = nextId;
+
+    // add guest
+    guestModel.insertMany(guest);
+
+    // create response
+    const response = {
+        responsecode: "1000",
+        responsemessage: "user added successfully",
+      };
+
+      res.send(response);
 })
 
 // export router
