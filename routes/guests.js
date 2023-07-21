5065cba44fe9aaa16b816427d8857245739f0f09
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-21 17:54:00 +0300

    Get all guests done

--- routes/guests.js
+++ routes/guests.js
@@ -1,33 +1,41 @@
 // import express
-const express = require('express');
+const express = require("express");
 
 // get model
-const guestModel = require('../models/guestmodel');
-
+const guestModel = require("../models/guestmodel");
 
 // defn=ine router
 const guestRouter = express.Router();
 
 // add guest
-guestRouter.post("/", async function(req, res, next){
-    // get the number of existing guests
-    const nextId = await guestModel.count() + 1;
+guestRouter.post("/", async function (req, res, next) {
+  // get the number of existing guests
+  const nextId = (await guestModel.count()) + 1;
 
-    // get the body
-    const guest = req.body;
-    guest.id = nextId;
+  // get the body
+  const guest = req.body;
+  guest.id = nextId;
 
-    // add guest
-    guestModel.insertMany(guest);
+  // add guest
+  guestModel.insertMany(guest);
 
-    // create response
-    const response = {
-        responsecode: "1000",
-        responsemessage: "user added successfully",
-      };
+  // create response
+  const response = {
+    responsecode: "1000",
+    responsemessage: "user added successfully",
+  };
 
-      res.send(response);
-})
+  res.send(response);
+});
+
+// get all guests
+guestRouter.get("/", async (req, res) => {
+  // get all the guests
+  const guests = await guestModel.find();
+
+  // return guests
+  res.json(guests);
+});
 
 // export router
 module.exports = guestRouter;
