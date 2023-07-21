c541a55ffec8393a5a62a22e5cfab11aac147fa2
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-21 18:33:13 +0300

    Create parking model done

---
+++ models/parkingModel.js
@@ -0,0 +1,16 @@
+// get mongoose
+const { default: mongoose } = require("mongoose");
+
+// define a parking model
+const parkingSchema = new mongoose.Schema({
+    "building":{type: String ,required: true},
+    "location":{type: String ,required: true},
+    "number":{type: String ,required: true},
+    "available":{type: Boolean ,required: true},
+});
+
+// create a model
+const parkingModel = mongoose.model("parking", parkingSchema);
+
+// export module
+module.exports = parkingModel;
