a3417fa9a85e67fafc41a1bee4828dd0f0f25673
jkmacharia12 <mashjosephk01@gmail.com>
2023-07-23 14:03:38 +0300

    Edit and delete event added

--- routes/events.js
+++ routes/events.js
@@ -1,27 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const eventModel = require('../models/eventModel');
+const eventModel = require("../models/eventModel");
 
-   const router= express.Router();
-  
-   //get all events query
-   router.get('/', async function(req, res, next){
+const router = express.Router();
+
+//get all events query
+router.get("/", async function (req, res, next) {
   const events = await eventModel.find();
-    res.json(events);
+  res.json(events);
 });
-// router.get('/', async function(req, res, next) { 
-//   const users = await usermodel.find();
-//     res.json(users);
-//   });
+
+
 //find event by ID
 router.get("/:id", async (req, res) => {
   // define search id
   const searchId = req.params.id;
 
-  // get one guest
+  // get one event
   const event = await eventModel.find({ id: searchId });
 
-  // return guests
+  // return event
   res.json(event);
 });
 
@@ -45,5 +43,51 @@ router.post("/", async function (req, res, next) {
 
   res.send(response);
 });
+// get one Event
+router.delete("/:id", async (req, res) => {
+  // define response
+  let response = null;
+  // define delete id
+  const deleteId = req.params.id;
 
-module.exports = router
+  // check if event there
+  const event = await eventModel.find({ id: deleteId });
+
+  if (event.length < 1) {
+    response = {
+      responsecode: "4000",
+      responsemessage: "Event not found for deleting",
+    };
+  } else {
+    // delete an event
+    await eventModel.deleteOne({ id: deleteId });
+    response = {
+      responsecode: "1000",
+      responsemessage: "Event deleted successfully",
+    };
+  }
+  res.send(response);
+});
+
+// edit a event
+// add guest
+router.put("/:id", async function (req, res, next) {
+  // define edit id
+  const editId = req.params.id;
+
+  // get the body
+  const event = req.body;
+
+  // add guest
+  await eventModel.updateOne({ id: editId }, event);
+
+  // create response
+  const response = {
+    responsecode: "1000",
+    responsemessage: "Event updated successfully",
+  };
+
+  res.send(response);
+});
+
+module.exports = router;
